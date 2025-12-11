3e8d8fb6f6eaf5f87313b1ecaebc7541c6124b91
Steve Edson <steve@spacestardigital.com>
2025-12-11 20:02:36 +0000

    Add ShowEntriesInDB command and workflow step
    Introduces a new Artisan command app:show-entries to display entries from the database. Updates the GitHub Actions workflow to run this command for visibility into current DB entries during CI.

---
+++ app/Console/Commands/ShowEntriesInDB.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class ShowEntriesInDB extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:show-entries';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $entries = DB::table('entries')->get();
+        $this->info('There are ' . $entries->count() . ' entries in the database:');
+        foreach ($entries as $entry) {
+            $this->line('ID: ' . $entry->id . ', Slug: ' . $entry->slug . ', Collection: ' . $entry->collection . ', Site: ' . $entry->site);
+        }   
+    }
+}
